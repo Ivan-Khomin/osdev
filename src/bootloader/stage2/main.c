9a7393b9ab42c539830443bac195b15e5185dba8
Ivan-Khomin <homin151210@gmail.com>
2025-08-20 11:53:27 +0000

    Finish bootloader & implement kernel

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -1,8 +1,14 @@
 #include <stdint.h>
 #include "stdio.h"
 #include "disk.h"
+#include "fat.h"
+#include "memdefs.h"
+#include "memory.h"
 
-void* g_data = (void*)0x20000;
+uint8_t* KernelLoadBuffer = (uint8_t*)MEMORY_LOAD_KERNEL;
+uint8_t* Kernel = (uint8_t*)MEMORY_KERNEL_ADDR;
+
+typedef void (*KernelStart)();
 
 void __attribute__((cdecl)) start(uint16_t bootDrive)
 {
@@ -15,9 +21,26 @@ void __attribute__((cdecl)) start(uint16_t bootDrive)
         goto end;
     }
 
-    DISK_ReadSectors(&disk, 0, 1, g_data);
+    if (!FAT_Initialize(&disk))
+    {
+        printf("Cannot initialize FAT!\n");
+        goto end;
+    }
 
-    print_buffer("Boot sector: ", g_data, 512);
+    // laod kernel
+    FAT_File* fd = FAT_Open(&disk, "/kernel.bin");
+    uint32_t read;
+    uint8_t* kernelBuffer = Kernel;
+    while ((read = FAT_Read(&disk, fd, MEMORY_LOAD_SIZE, KernelLoadBuffer)))
+    {
+        memcpy(kernelBuffer, KernelLoadBuffer, read);
+        kernelBuffer += read;
+    }
+    FAT_Close(fd);
+
+    // execute kernel
+    KernelStart kernelStart = (KernelStart)Kernel;
+    kernelStart();
 
 end:
     for (;;);
