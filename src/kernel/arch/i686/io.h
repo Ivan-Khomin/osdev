d4a8c8a593e05500a9c8e419d15df83963992608
Ivan-Khomin <homin151210@gmail.com>
2025-11-14 17:28:58 +0000

    Making part 10

--- src/kernel/arch/i686/io.h
+++ src/kernel/arch/i686/io.h
@@ -4,5 +4,8 @@
 
 void __attribute__((cdecl)) i686_outb(uint16_t port, uint8_t value);
 uint8_t __attribute__((cdecl)) i686_inb(uint16_t port);
+void __attribute__((cdecl)) i686_EnableInterrupts();
+void __attribute__((cdecl)) i686_DisableInterrupts();
 
+void i686_iowait();
 void __attribute__((cdecl)) i686_Panic();
